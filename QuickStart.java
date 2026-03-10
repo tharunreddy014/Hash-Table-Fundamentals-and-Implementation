355fe486728469d2229fa1f1eb0c52177f42a174
tharunreddy014 <ammireddytharunkumarreddy@gmail.com>
2026-03-10 15:19:40 +0530

    commit changes

---
+++ QuickStart.java
@@ -0,0 +1,15 @@
+public class QuickStart {
+    public static void main(String[] args) {
+    
+        String language = "Java";
+        int version = 21; 
+
+       
+        System.out.println("Hello! You are running " + language + " version " + version);
+
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Iteration: " + i);
+        }
+    }
+}
